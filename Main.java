6e5f8f0e7dd9639dc40c11ff31cb24346d49b1d8
vlad <vladislav.masentsov@yandex.ru>
2026-02-01 02:30:08 +0300

    new

--- Main.java
+++ Main.java
@@ -2,26 +2,56 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
-        Fisrt fisrt_ex = new Fisrt("null", "null");
-        fisrt_ex.Print();
+        var example = new Fisrt[3];
+
+        example[0] = new Fisrt("1", "2");
+        example[1] = new Fisrt("3", "4");
+        example[2] = new Fisrt("5", "6");
+
+        for (Fisrt e: example)
+            e.Print();
+
+        for (Fisrt e: example)
+            System.out.println(e.getCounter());
+
 
     }
-
-
 }
-
 
 class Fisrt{
     private String name;
     private String value;
+    private Integer counter;
+    public static Integer id = 1;
 
     public Fisrt(String name, String value){
         this.name = name;
         this.value = value;
     }
 
+    public void setCounter(Integer counter) {
+        this.counter = counter;
+    }
+
+    public String getCounter(){
+        if (this.counter == null)
+            {
+                return "Crash";
+            }
+        else
+        {
+            return this.counter.toString();
+        }
+
+    }
+
+    public void PlusCounter(){
+        this.counter++;
+        System.out.println(this.counter);
+    }
+
     public void Print(){
-        System.out.println(this.name);        
+        System.out.println(this.name + "__" + this.value);
     }
 
 }
