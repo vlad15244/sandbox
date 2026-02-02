ecdf35aff1b9e12cc9748ab209f31d90067560c5
vlad <vladislav.masentsov@yandex.ru>
2026-02-03 00:25:44 +0300

    new

--- Main.java
+++ Main.java
@@ -1,4 +1,11 @@
+import javax.swing.*;
+import java.time.*;
+import java.util.ArrayList;
+
 public class Main {
+
+
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
@@ -8,13 +15,30 @@ public class Main {
         example[1] = new Fisrt("3", "4");
         example[2] = new Fisrt("5", "6");
 
-        for (Fisrt e: example)
+        for (Fisrt e : example)
             e.Print();
 
-        for (Fisrt e: example)
+        for (Fisrt e : example)
             System.out.println(e.getCounter());
 
+        if (example[0] instanceof Fisrt) {
+            System.out.println("This is instance");
+        }
+        var worker1 = new Worker("vlad", "null");
+        System.out.println(worker1.getDescription());
+        var workers = new ArrayList<Worker>();
+        workers.add(new Worker("jack", "nil"));
+        Worker w1 = workers.get(0);
+        System.out.println(w1.frm(1.0,2.0,3.0));
+        workers.add(new Worker("bill", "oops"));
 
+        for (Worker w : workers)
+            System.out.println(w.getDescription());
+
+        workers.remove(1);
+
+        for (Worker w : workers)
+            System.out.println(w.getDescription());
     }
 }
 
@@ -54,4 +78,71 @@ class Fisrt{
         System.out.println(this.name + "__" + this.value);
     }
 
+}
+
+class Second extends Fisrt{
+
+    public Second(String name, String value) {
+        super(name, value);
+        this.bonus = 0;
+    }
+
+    private double bonus;
+
+    public void setBonus(double b){
+        this.bonus = b;
+
+    }
+
+    public double getBonus(){
+        return this.bonus;
+    }
+
+    @Override
+    public String getCounter() {
+        return super.getCounter();
+    }
+}
+
+final class Third extends Fisrt{
+    public Third(String name, String value){
+        super(name, value);
+
+    }
+}
+
+abstract class Person{
+    private String name;
+
+    public Person(String name)
+    {
+        this.name = name;
+    }
+
+    public abstract String getDescription();
+
+    public String getName(){
+        return this.name;
+    }
+}
+
+class Worker extends Person{
+    private String magor;
+
+    public Worker(String name, String major){
+        super(name);
+        this.magor = major;
+    }
+
+    public String getDescription(){
+        return "a worker " + this.getName() + " in " + this.magor;
+    }
+
+    public String frm(Double... values){
+        String result = "";
+        for (Double v : values){
+            result = result + v.toString();
+        }
+        return result.toString();
+    }
 }
--- join_.py
+++ join_.py
@@ -51,6 +51,10 @@ class Table():
         self.parent_table = parent_table
         self.database_obj = database_obj
 
+
+    def __str__(self):
+        return self.name
+
     def AddColumn(self, column):
         self.Columns.append(column) 
 
@@ -83,75 +87,175 @@ class Table():
         self.database_obj.connection.close()
 
 
+    def insert_one(self, value : dict) -> bool:
+
+        aQuery = 'INSERT INTO ' + self.name + ' ('
+        if not value:
+            pass 
+            """Дбавить обработку ошибки"""
+          
+        aCol = ''
+        aVal = ''
+
+        #подготовка запроса
+        for col in self.Columns:
+            if 'ID' in col.name and 'AUTO_INCREMENT' in col.additional_params:
+                continue
+            else:
+                aCol += col.name + ','
+                aVal += '%s,'
+
+        aCol = aCol[:-1]
+        aVal = aVal[:-1]        
+
+        aQuery += aCol + ') VALUES ('
+        aQuery += aVal +');'
+
+        self.database_obj.connect()
+
+        cursor = self.database_obj.connection.cursor()
+        cursor.execute(aQuery, (value,))
+        self.database_obj.connection.commit()
+
+    def insert_many(self, value : dict) -> bool:
+
+        aQuery = 'INSERT INTO ' + self.name + ' ('
+        if not value:
+            pass 
+            """Дбавить обработку ошибки"""
+          
+        aCol = ''
+        aVal = ''
+
+        #подготовка запроса
+        for col in self.Columns:
+            if 'ID' in col.name and 'AUTO_INCREMENT' in col.additional_params:
+                continue
+            else:
+                aCol += col.name + ','
+                aVal += '%s,'
+
+        aCol = aCol[:-1]
+        aVal = aVal[:-1]        
+
+        aQuery += aCol + ') VALUES ('
+        aQuery += aVal +');'
+
+        self.database_obj.connect()
+
+        cursor = self.database_obj.connection.cursor()
+        cursor.executemany(aQuery, (value))
+        self.database_obj.connection.commit()
+
+    def select_all(self) -> dict:
+
+        aQuery = 'SELECT * FROM ' + self.name + ';'
+        self.database_obj.connect()
+
+        cursor = self.database_obj.connection.cursor()
+        cursor.execute(aQuery)
+
+        return cursor.fetchall()
+    
+    def select_filter(self, filter: dict) -> dict:
+
+        aQuery = 'SELECT * FROM ' + self.name + ' WHERE '
+        where_parts = []
+
+        for flt in filter:
+
+            before, sep, after = flt.partition('_')
+            before1, sep1, after1 = flt.partition('=')            
+            if '__eq' in flt:
+                where_parts.append(f"{before} = '{after1}'")
+            if '__lt' in flt:
+                where_parts.append(f"{before} > '{after1}'")
+            if '__gt' in flt:
+                where_parts.append(f"{before} < '{after1}'")
+            if '__le' in flt:
+                where_parts.append(f"{before} >= '{after1}'")
+            if '__ge' in flt:
+                where_parts.append(f"{before} <= '{after1}'")
+
+        aQuery += ' , '.join(where_parts)
+
+        self.database_obj.connect()
+
+        cursor = self.database_obj.connection.cursor()
+        cursor.execute(aQuery)
+
+        return cursor.fetchall()
     
 
-def join_table(table1, table2, filter = None, order_by = None):
-    query = 'SELECT * FROM ' + table1.name + ' JOIN ' + table2.name + ' ON ' + table1.name +'.' 
+    def join_table(self, filter = None, order_by = None):
+        query = 'SELECT * FROM ' + str(self.parent_table) + ' JOIN ' + self.name + ' ON ' + self.name +'.'
 
-    for col in table1.Columns:
-        if 'ID' in col.name and 'AUTO_INCREMENT' in col.additional_params: 
-            query += col.name
-            break 
+        if  self.parent_table:
+            query += self.foreign_key
 
-    query += ' = ' + table2.name +'.'
+        query += ' = ' + str(self.parent_table) +'.'  
 
-    if table2.parent_table:
-        query += table2.foreign_key
+        for col in self.Columns:
+            if 'ID' in col.name and 'AUTO_INCREMENT' in col.additional_params: 
+                query += col.name
+                break 
 
-    where_parts = []
+        where_parts = []
 
-    if filter:
-        query += ' WHERE '
+        if filter:
+            query += ' WHERE '
 
-        for col in table2.Columns:
-            for flt in filter:
-                if col.name in flt:
-                    before, sep, after = flt.partition('=')
-                    if '__eq' in flt:
-                        where_parts.append(f"{table2.name}.{col.name} = {after}")
-                    if '__lt' in flt:
-                        where_parts.append(f"{table2.name}.{col.name} > {after}")
-                    if '__gt' in flt:
-                        where_parts.append(f"{table2.name}.{col.name} < {after}")
-                    if '__lte' in flt:
-                        where_parts.append(f"{table2.name}.{col.name} >= {after}")
-                    if '__gte' in flt:
-                        where_parts.append(f"{table2.name}.{col.name} <= {after}")
-                    if '__btw' in flt:
-                        val1, sep1, val2 = after.partition('^')
-                        where_parts.append(f"({table2.name}.{col.name} BETWEEN {val1} AND {val2})")                        
+            for col in self.Columns:
+                for flt in filter:
+                    if col.name in flt:
+                        before, sep, after = flt.partition('=')
+                        if '__eq' in flt:
+                            where_parts.append(f"{self.name}.{col.name} = {after}")
+                        if '__lt' in flt:
+                            where_parts.append(f"{self.name}.{col.name} > {after}")
+                        if '__gt' in flt:
+                            where_parts.append(f"{self.name}.{col.name} < {after}")               
+                        if '__le' in flt:
+                            where_parts.append(f"{self.name}.{col.name} >= {after}")
+                        if '__ge' in flt:
+                            where_parts.append(f"{self.name}.{col.name} <= {after}")
+                            
+        query += ' AND '.join(where_parts)
 
-    query += ' AND '.join(where_parts)
+        order_parts = []
 
-    order_parts = []
+        if order_by:
 
-    if order_by:
+            query += ' ORDER BY '
+            for col in self.parent_table.Columns:
+                for ord in order_by:
+                    if col.name in ord:
+                        if '__asc' in ord:
+                            order_parts.append(f"{col.name} ASC")
+                        if '__dsc' in ord:
+                            order_parts.append(f"{col.name} DSC")
 
-        query += ' ORDER BY '
-        for col in table2.Columns:
-            for ord in order_by:
-                if col.name in ord:
-                    if '__asc' in ord:
-                        order_parts.append(f"{col.name} ASC")
-                    if '__dsc' in ord:
-                        order_parts.append(f"{col.name} DSC")
+        query += ' , '.join(order_parts) 
+        self.database_obj.connect()
+        cursor = self.database_obj.connection.cursor()
+        cursor.execute(query)
 
-    query += ' , '.join(order_parts)            
-    return query
+        return cursor.fetchall()        
+ 
+
 
 if __name__ == "__main__":
 
     MySQL = Run_Database('root', '1234','localhost', 'test')
-
 
     Table_Order = Table('orders', MySQL)
 
     Table_Order.AddColumn(Column('ID', 'BIGINT', 'UNSIGNED NOT NULL AUTO_INCREMENT'))
     Table_Order.AddColumn(Column('NAME', 'VARCHAR(40)', 'NOT NULL'))
 
-
     Table_Order.Initialization()
 
+    Table_Order.select_filter(('ID__le=2',))
     Table_Order1 = Table('items',MySQL, Table_Order)
 
     Table_Order1.AddColumn(Column('ID', 'BIGINT', 'UNSIGNED NOT NULL AUTO_INCREMENT'))
@@ -160,6 +264,4 @@ if __name__ == "__main__":
 
 
     Table_Order1.Initialization()
-
-
-    
+    print(Table_Order1.join_table(('ID__eq=2',)))
